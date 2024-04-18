d9c49dce86379006f52605fb4a09500b6aff1f6e
LukaMelinc <92365296+LukaMelinc@users.noreply.github.com>
2024-04-18 17:45:44 +0200

    Add files via upload

--- img_show.c
+++ img_show.c
@@ -2,14 +2,32 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+
 int main() {
-    int read = open("izhod.raw", O_RDONLY);
+
+ 
+
+    int raw_slika = open("izhod.raw", O_RDONLY);
     int show = open("/dev/fb0", O_WRONLY);
 
-   
+   //off_t lseekm_stat =  lseek(int fd, off_t offset, int whence);
+    off_t zacentni_odmik = lseek(show, 0, SEEK_CUR);
+    char buffer[142000];
+    int slika = read(raw_slika, buffer, sizeof(buffer));
 
-   
-    close(read);
+    if (slika > 0){
+        write(show, buffer, slika);
+    }
+
+
+    off_t koncna_pozicija = lseek(show, 0, SEEK_CUR);
+
+    off_t pozicija_slike = koncna_pozicija - zacentni_odmik;
+
+    lseek(show, pozicija_slike, SEEK_SET);
+
+
+    close(raw_slika);
     close(show);
 
     return 0;
